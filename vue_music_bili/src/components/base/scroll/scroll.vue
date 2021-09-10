ba081867527f2f07993282112231490062d37c8c
miaomiao-chong <1330293737@qq.com>
2021-09-10 16:48:52 +0800

    'scroll'

---
+++ vue_music_bili/src/components/base/scroll/scroll.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="scroll">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import BScroll from "@better-scroll/core";
+import ObserveDOM from "@better-scroll/observe-dom";
+
+import ScrollBar from "@better-scroll/scroll-bar";
+BScroll.use(ScrollBar);
+BScroll.use(ObserveDOM);
+export default {
+  name: "scroll",
+
+  mounted() {
+    new BScroll(this.$refs.scroll, {
+      scrollY: true,
+      // scrollbar: true
+      observeDOM: true 
+    });
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- vue_music_bili/src/views/recommend.vue
+++ vue_music_bili/src/views/recommend.vue
@@ -1,29 +1,35 @@
 <template>
-  <div>
-    <div class="slider-container">
-      <Slider :sliders="banners" v-if="banners.length"></Slider>
-    </div>
-    <div class="playlist-container">
-      <h1 class="title">{{ title }}</h1>
-      <div class="playlist" v-for="item in playlist" :key="item.id">
-        <div class="playlist-item">
-          <img v-lazy="item.coverImgUrl" alt="" class="pic" />
-          <div class="text">
-            <div class="text1">{{ item.name }}</div>
-            <div class="text2">{{ item.copywriter }}</div>
+  <div class="recommend-container">
+    <Scroll class="scroll-container">
+      <div>
+        <div class="slider-container">
+          <Slider :sliders="banners" v-if="banners.length"></Slider>
+        </div>
+        <div class="playlist-container">
+          <h1 class="title">{{ title }}</h1>
+          <div class="playlist" v-for="item in playlist" :key="item.id">
+            <div class="playlist-item">
+              <img v-lazy="item.coverImgUrl" alt="" class="pic" />
+              <div class="text">
+                <div class="text1">{{ item.name }}</div>
+                <div class="text2">{{ item.copywriter }}</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Scroll>
   </div>
 </template>
 
 <script>
 import { getBanner, getPlaylist } from "../server/recommend";
 import Slider from "@/components/base/slider/slider";
+import Scroll from "@/components/base/scroll/scroll";
+
 export default {
   name: "recommend",
-  components: { Slider },
+  components: { Slider, Scroll },
   data() {
     return {
       banners: [],
@@ -43,50 +49,62 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.slider-container {
+.recommend-container{
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+.scroll-container {
+  // height: 530px;
+  height: 100%;
   overflow: hidden;
-}
-.playlist-container {
-  .title {
-    color: $color-theme;
-    text-align: center;
-    margin: 10px 0;
+  .slider-container {
+    overflow: hidden;
   }
-  .playlist {
-    .playlist-item {
-      display: flex;
-      // justify-content: center;
-      align-items: center;
+  .playlist-container {
+    .title {
+      color: $color-theme;
+      text-align: center;
       margin: 10px 0;
-      .pic {
-        width: 50px;
-        margin: 0 15px 0 19px;
-      }
-      .text {
-        height: 40px;
-        position: relative;
-        font-size: 8px;
-        // flex: 1;
-        width: 260px;
-
-        .text1 {
-          width:100%;
-          color: rgb(253, 253, 253);
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
+    }
+    .playlist {
+      .playlist-item {
+        display: flex;
+        // justify-content: center;
+        align-items: center;
+        margin: 10px 0;
+        .pic {
+          width: 50px;
+          margin: 0 15px 0 19px;
         }
-        .text2 {
-          width: 100%;
-          position: absolute;
-          bottom: 0px;
-          color: rgba(234, 237, 240, 0.801);
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
+        .text {
+          height: 40px;
+          position: relative;
+          font-size: 8px;
+          // flex: 1;
+          width: 260px;
+
+          .text1 {
+            width: 100%;
+            color: rgb(253, 253, 253);
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+          }
+          .text2 {
+            width: 100%;
+            position: absolute;
+            bottom: 0px;
+            color: rgba(234, 237, 240, 0.801);
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+          }
         }
       }
     }
   }
 }
+}
+
 </style>
